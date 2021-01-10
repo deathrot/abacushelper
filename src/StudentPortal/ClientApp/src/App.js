3aa31d005f828f25c937d1337ac4cc0fcc41c101
Deepak Sharma <sharma.deepak83@gmail.com>
2021-01-09 23:33:09 -0500

    x

--- src/StudentPortal/ClientApp/src/App.js
+++ src/StudentPortal/ClientApp/src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/home';
-import { Login } from './components/login';
+import CreateAccount from './components/create_account';
+import Login from './components/login';
 import { AppContextProvider } from './context/app_context';
 
 import './custom.css'
 
-export default App = (props) => {
-    const displayName = App.name;
+const App = (props) => {
 
     return (
         <Layout>
             <AppContextProvider>
-                <Route exact path='/' component={Home} />
+                <Route exact path='/' component={CreateAccount}  />
                 <Route path='/login' component={Login} />
             </AppContextProvider>
         </Layout>
     );
-}
+};
+
+export default App;
+
--- src/StudentPortal/ClientApp/src/components/create_account.js
+++ src/StudentPortal/ClientApp/src/components/create_account.js
@@ -1,9 +1,63 @@
 import React, {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import './create_account.css';
+import _ from 'lodash';
+import './common.css';
 
 const CreateAccount = (props) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = (e) => {
+
+        e.preventdefault();
+    }
 
     return (
-        <h3>Create Account</h3>
+        <div>
+            <h3>Create account</h3>
+            <div class="input">
+                <TextField label="Name:"
+                margin="normal"
+                style={{ margin: 8 }}
+                InputLabelProps={{
+                    shrink: true,
+                  }}
+                fullWidth
+                value={email} onChange={(e) => setEmail(e.target.value)} 
+                helperText="John Smith" variant="outlined"></TextField>
+            </div>
+            <div class="input">
+                <TextField label="Email:"
+                margin="normal"
+                type="email"
+                style={{ margin: 8 }}
+                InputLabelProps={{
+                    shrink: true,
+                  }}
+                fullWidth
+                value={email} onChange={(e) => setEmail(e.target.value)} 
+                helperText="john@gmail.com" variant="outlined"></TextField>
+            </div>
+            <div class="input">
+                <TextField label="Password:"
+                margin="normal"
+                type="password"
+                style={{ margin: 8 }}
+                InputLabelProps={{
+                    shrink: true,
+                  }}
+                fullWidth
+                value={name} onChange={(e) => setName(e.target.value)} 
+                helperText="minimum length 6, atleast 1 uppercase" variant="outlined"></TextField>
+            </div>
+            <div class="input">
+                <Button variant="contained" style={{margin: 8}} onClick={(e) => handleRegister(e)} color="primary">Register</Button>
+            </div>
+        </div>
     );
 }
 
